577fc873a272f402228d5040cfecb6e05afb19d7
ValeraGlebov <75813652+ValeraGlebov@users.noreply.github.com>
2021-01-08 12:00:21 +0200

    added system property - browser

--- src/test/java/com/example/illcarro/qa/tests/TestBase.java
+++ src/test/java/com/example/illcarro/qa/tests/TestBase.java
@@ -1,9 +1,11 @@
 package com.example.illcarro.qa.tests;
 import com.example.illcarro.qa.application.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.*;
 
 public class TestBase {
-    protected static ApplicationManager app = new ApplicationManager();
+    protected static ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp(){
